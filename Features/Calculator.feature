e31523218516591e57c34ec06c0fee70ef70e0d7
vip0720 <vish7.pujari@gmail.com>
2024-09-05 10:40:25 +0530

    Add project files.

---
+++ Features/Calculator.feature
@@ -0,0 +1,20 @@
+﻿Feature: Calculator
+![Calculator](https://specflow.org/wp-content/uploads/2020/09/calculator.png)
+Simple calculator for adding **two** numbers
+
+Link to a feature: [Calculator](PlayWrightSpecFlow/Features/Calculator.feature)
+***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
+
+@mytag
+Scenario: Add two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+
+@mytag
+Scenario: Launching Google and searching for Arsenal
+	Given Launch the google website
+	When Search for 'Arsenal'
+	Then User is able to see results for Arsenal
+	 
---
+++ StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,65 @@
+using Microsoft.Playwright;
+
+namespace PlayWrightSpecFlow.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            throw new PendingStepException();
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+
+            throw new PendingStepException();
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            //TODO: implement act (action) logic
+
+            throw new PendingStepException();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            //TODO: implement assert (verification) logic
+
+            throw new PendingStepException();
+        }
+
+        [Given(@"Launch the google website")]
+        public void GivenLaunchTheGoogleWebsite()
+        {
+            
+        }
+
+        [When(@"Search for '([^']*)'")]
+        public void WhenSearchFor(string arsenal)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"User is able to see results for Arsenal")]
+        public void ThenUserIsAbleToSeeResultsForArsenal()
+        {
+            throw new PendingStepException();
+        }
+
+    }
+}
